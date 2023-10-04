05b96e260f393cc8f5fe7016547a8063ce09f4b7
Ramiro <ramiro174@gmail.com>
2023-10-04 10:03:56 -0600

    agrugue una variable

--- app/src/main/java/com/example/gitprueba/MainActivity.java
+++ app/src/main/java/com/example/gitprueba/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.gitprueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button B;
     }
 }
